65722a64142d37ac57417ff6261c15d2bcd6bb77
indreshgit10 <indreshkumar456789@gmail.com>
2025-10-05 22:20:29 +0530

    Time: 0 ms (100%), Space: 9.4 MB (55.64%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,8 +3,8 @@ public:
     bool isIsomorphic(string s, string t) {
         if(s.size() != t.size()) return false;
         
-        unordered_map<char, char> m1; // s -> t
-        unordered_map<char, char> m2; // t -> s
+        unordered_map<char, char> m1; 
+        unordered_map<char, char> m2;
         
         for(int i = 0; i < s.size(); i++) {
             char cs = s[i], ct = t[i];
