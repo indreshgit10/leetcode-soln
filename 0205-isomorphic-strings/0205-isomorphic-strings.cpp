8f0684605be1a04a269f6ad093898ac74e1b6af1
indreshgit10 <indreshkumar456789@gmail.com>
2025-10-05 22:20:10 +0530

    Time: 5 ms (12.96%), Space: 9.4 MB (28.9%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        unordered_map<char, char> m1; // s -> t
+        unordered_map<char, char> m2; // t -> s
+        
+        for(int i = 0; i < s.size(); i++) {
+            char cs = s[i], ct = t[i];
+            
+            if(m1.count(cs) && m1[cs] != ct) return false;
+            if(m2.count(ct) && m2[ct] != cs) return false;
+            
+            m1[cs] = ct;
+            m2[ct] = cs;
+        }
+        return true;
+    }
+};
