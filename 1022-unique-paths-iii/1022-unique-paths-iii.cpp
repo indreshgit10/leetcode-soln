83fcbd171e72764fa95d42ff5c903e804bb12ba7
indreshgit10 <indreshkumar456789@gmail.com>
2025-10-12 08:03:33 +0530

    Time: 0 ms (100%), Space: 9.3 MB (76.61%) - LeetHub

---
+++ 1022-unique-paths-iii/1022-unique-paths-iii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int ans = 0;
+    int totalEmpty = 0;
+    int startX, startY;
+
+    void dfs(vector<vector<int>>& grid, int x, int y, int remain) {
+        if (x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size() ||
+            grid[x][y] == -1)
+            return;
+
+        if (grid[x][y] == 2) {
+            if (remain == 0)
+                ans++;
+            return;
+        }
+
+        grid[x][y] = -1;
+
+        dfs(grid, x + 1, y, remain - 1);
+        dfs(grid, x - 1, y, remain - 1);
+        dfs(grid, x, y + 1, remain - 1);
+        dfs(grid, x, y - 1, remain - 1);
+
+        grid[x][y] = 0;
+    }
+
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != -1)
+                    totalEmpty++;
+                if (grid[i][j] == 1) {
+                    startX = i;
+                    startY = j;
+                }
+            }
+        }
+        dfs(grid, startX, startY, totalEmpty - 1);
+        return ans;
+    }
+};
