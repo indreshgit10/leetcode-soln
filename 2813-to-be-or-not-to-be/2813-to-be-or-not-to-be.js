05dd170398f33f4197c1b5fed7ad5647b77285c8
indreshgit10 <indreshkumar456789@gmail.com>
2025-10-20 23:07:13 +0530

    Time: 56 ms (7.31%), Space: 54.4 MB (14.83%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,13 @@
+
+var expect = function(val) {
+    return {
+        toBe: function(val2) {
+            if (val === val2) return true;
+            throw "Not Equal";
+        },
+        notToBe: function(val2) {
+            if (val !== val2) return true;
+            throw "Equal";
+        }
+    }
+};
