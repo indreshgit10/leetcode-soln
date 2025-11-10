2fecd0826c148e512bd07702c89665c9ee225a9a
indreshgit10 <indreshkumar456789@gmail.com>
2025-11-10 06:25:34 +0530

    Time: 37 ms (5.13%), Space: 11.1 MB (19%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+        int l=0;
+        int r=0;
+        int maxl=0;
+        int maxf=0;
+
+        unordered_map<char,int> m;
+
+        while(r<s.size()){
+
+            m[s[r]-'A']++;
+            maxf= max(maxf, m[s[r]-'A']);
+
+            while((r-l+1- maxf) >k){
+                 m[s[l]-'A']--;
+                 maxf=0;
+
+                 for(int i=0; i<25; i++){
+                    maxf= max(maxf, m[i]);
+                 }
+                 l++;
+            }
+
+            if(r-l+1 - maxf<=k){
+                maxl= max(maxl, r-l+1);
+            }
+            r++;
+        }
+        return maxl;
+    }
+};
